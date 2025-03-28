ff58d81ad58d57bbadd918be3b4c78b05a396355
xGood <zenia2003725@gmail.com>
2025-03-28 18:25:57 +0300

    Upgraded client programm

---
+++ 20250324/1/client.py
@@ -0,0 +1,158 @@
+import cmd
+import sys
+import socket
+import shlex
+import json
+import cowsay
+import readline
+
+
+WEAPONS_LIST = {"sword": 10, "spear": 15, "axe": 20}
+dflt_wpn = "sword"
+MONSTER_DICT = {}
+LOGINED_USERS = set()
+
+
+def encounter(name, msg):
+    print(cowsay.cowsay(msg, cow=name))
+
+
+class MessageParser():
+
+    @classmethod
+    def parse(msg: str):
+        """Processes upcoming message"""
+        if not msg.startswith("srv"):
+            print(f"{msg}{cmd_line.prompt}{readline.get_line_buffer()}", end="", flush=True)
+        else:
+            cmd = msg.split()
+            #todo: implement
+            return
+
+
+class cmd_line(cmd.Cmd):
+
+    def __init__(self, sock):
+        self.socket = sock
+        super().__init__()
+
+    prompt = "(MUD) "
+
+    def do_up(self, args):
+        self.socket.send("move up".encode())
+        data = self.socket.recv(1024).decode().split('\n')
+        print(data[0])
+        if data[1].startswith("Found"):
+            encounter(*data[1].split()[1:])
+        #plr.move("up")
+    
+    def do_down(self, args):
+        self.socket.send("move down".encode())
+        data = self.socket.recv(1024).decode().split('\n')
+        print(data[0])
+        if data[1].startswith("Found"):
+            encounter(*data[1].split()[1:])
+        #plr.move("down")
+    
+    def do_left(self, args):
+        self.socket.send("move left".encode())
+        data = self.socket.recv(1024).decode().split('\n')
+        print(data[0])
+        if data[1].startswith("Found"):
+            encounter(*data[1].split()[1:])
+        #plr.move("left")
+    
+    def do_right(self, args):
+        self.socket.send("move right".encode())
+        data = self.socket.recv(1024).decode().split('\n')
+        print(data[0])
+        if data[1].startswith("Found"):
+            encounter(*data[1].split()[1:])
+        #plr.move("right")
+    
+    def do_addmon(self, args):
+        global MONSTER_DICT
+        try:
+            name, *rules = shlex.split(args)
+            hello_ind = rules.index("hello")
+            hp_ind = rules.index("hp")
+            coords_ind = rules.index("coords")
+            x, y = int(rules[coords_ind+1]), int(rules[coords_ind+2])
+            hp = int(rules[hp_ind+1])
+            hello = rules[hello_ind+1]
+            try:
+                x, y = int(x), int(y)
+            except:
+                print("Invalid arguments")
+                return
+            if name not in [*cowsay.list_cows(), "jgsbat"]:
+                print("Cannot add unknown monster")
+                return
+            if x < 0 or y < 0 or x >= field_x or y >= field_y or not (hasattr(hello, "__str__") or hasattr(hello, "__repr__")):
+                print("Invalid arguments")
+                return
+        except:
+            print("Invalid arguments")
+            return
+        self.socket.sendall(f"addmon {x} {y} {hp} {name} {hello}".encode())
+        data = self.socket.recv(1024).decode()
+        data = data.split('\n')
+        MONSTER_DICT = json.loads(data[0])
+        print(data[1])
+        #fld.addmon(x, y, hp, name, hello)
+    
+    def do_attack(self, args):
+        try:
+            name = shlex.split(args)[0]
+        except:
+            print("Invalid command")
+            return
+        if "with" in args:
+            try:
+                name, _, weapon = shlex.split(args)
+                if WEAPONS_LIST.get(weapon, None) is None:
+                    print("Unknown weapon")
+                    return
+                print("kekw")
+                self.socket.send(f"attack {name} {WEAPONS_LIST[weapon]}".encode())
+                print("kekw")
+                data = self.socket.recv(1024).decode().split('\n')
+                print(data[0])
+                if (data[1] != ""):
+                    print(data[1])
+                #plr.attack(name, WEAPONS_LIST[weapon])
+            except ValueError:
+                print("Need to specify the weapon")
+                return
+        else:
+            self.socket.send(f"attack {name} {WEAPONS_LIST[dflt_wpn]}".encode())
+            data = self.socket.recv(1024).decode().split('\n')
+            print(data[0])
+            if (data[1] != ""):
+                print(data[1])
+            #plr.attack(name, WEAPONS_LIST[dflt_wpn])
+    
+    def complete_attack(self, text, line, ind1, ind2):
+        words = shlex.split(line)
+        if len(words) == 2:
+            return [c for c in MONSTER_DICT.keys() if MONSTER_DICT[c] > 0 and c.startswith(text)]
+        if len(words) < 3:
+            return []
+        if len(words) == 3:
+            return WEAPONS_LIST.keys()
+        return [c for c in WEAPONS_LIST.keys() if c.startswith(text)]
+    
+
+def receiver(conn):
+    while data := conn.recv(1024):
+        MessageParser.parse(data.decode())
+
+
+if __name__ == "__main__":
+    print("<<< Welcome to Python-MUD 0.1 >>>")
+    host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
+    port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        
+        cmd_line(sock=s).cmdloop()
