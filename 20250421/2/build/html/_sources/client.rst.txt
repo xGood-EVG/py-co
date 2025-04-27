90cae46b7ea62fe1ca1c22c05b6ab61ccea12376
xGood <zenia2003725@gmail.com>
2025-04-27 15:18:28 +0300

    Copied

---
+++ 20250421/2/build/html/_sources/client.rst.txt
@@ -0,0 +1,6 @@
+Client
+======
+
+.. automodule:: mood.client
+   :members:
+   :private-members:
---
+++ 20250421/2/mood/server/__main__.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from server import *
+
+if __name__ == "__main__":
+    start_server()
